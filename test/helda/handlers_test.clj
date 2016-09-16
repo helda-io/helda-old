2d3695be7ba552e6e0cc04f8cd39a9caf8ea48a1
legkvla <vladimir.legkunets@gmail.com>
2016-09-16 21:35:02 +0300

    Fixing handlers test

--- test/helda/handlers_test.clj
+++ test/helda/handlers_test.clj
@@ -1,16 +1,18 @@
 (ns helda.handlers-test
   (:require [clojure.test :refer :all]
             [helda.examples.accounting :refer :all]
-            [helda.meta.handlers :refer :all]
+            [helda.meta.core :refer :all]
+            [helda.meta.handlers :as h]
             ))
 
 (deftest add-handler-test
-  (let [meta (add-handler {} {
-    :input-msg {:tag :msg-handler1 :txt "this is text msg"}
+  (let [meta (add-handler (init-meta :handler-test) {
+    :tag :msg-handler1
+    :input-msg {:txt "this is text msg"}
     :handler (fn [msg world] 2)
     })]
     (testing "Checking handler"
-      (is (= 2 (handle {:tag :msg.handler1} meta {:meta meta})))
+      (is (= 2 (h/handle {:tag :msg-handler1} meta {:meta meta})))
       )
     )
   )
