b5a2470b5bef2a2f7155d4859937752f55739eef
legkvla <vladimir.legkunets@gmail.com>
2016-09-18 17:29:41 +0300

    Enhancing schemas

--- src/helda/meta/schemas.clj
+++ src/helda/meta/schemas.clj
@@ -1,46 +1,47 @@
 (ns helda.meta.schemas
   (:require [schema.core :as s])
+  (:require [schema.core :refer [defschema =>]])
   )
 
-(s/defschema Field{
+(defschema Field{
     :name s/Keyword
     (s/optional-key :description) s/Str
     :default-value s/Any
   })
 
-(s/defschema Message{
+(defschema Message{
     (s/required-key :tag) s/Keyword
     s/Keyword s/Any
   })
 
-(s/defschema World{
+(defschema World{
     s/Keyword s/Any
   })
 
-(s/defschema Response{
+(defschema Response{
     :msg Message
     (s/optional-key :world) World
   })
 
-(s/defschema Generator{
+(defschema Generator{
     :period s/Num
     :count s/Num
-    :msg-source (s/=> Message []) ;function that provides msgs
+    :msg-source (=> Message []) ;function that provides msgs
   })
 
-(s/defschema Handler{
+(defschema Handler{
     :tag s/Keyword
     :input-msg {
       s/Keyword s/Str ;description
     }
-    :handler (s/=> Response [Message World])
+    :handler (=> Response [Message World])
     (s/optional-key :generator) Generator
-    (s/optional-key :validator) (s/=> (s/maybe Message) [Message])
+    (s/optional-key :validator) (=> (s/maybe Message) [Message])
   })
 
-(s/defschema Meta{
+(defschema Meta{
   :name s/Keyword
   :fields {s/Keyword Field}
   :handlers {s/Keyword Handler}
-  :sys-handlers {s/Keyword (s/=> Message [Message Meta])}
+  :sys-handlers {s/Keyword (=> Message [Message Meta])}
   })
