3a02124e9ed27f6dbfaf6974661f79bb2e15f956
legkvla <vladimir.legkunets@gmail.com>
2016-10-04 10:03:42 +0300

    Using pmap instead of thread

--- src/helda/assembly/generators.clj
+++ src/helda/assembly/generators.clj
@@ -1,27 +1,24 @@
 (ns helda.assembly.generators
-  (:require [clojure.core.async :refer [thread]])
   (:require [schema.core :as s])
   (:require [helda.assembly.schemas :refer :all])
   (:require [helda.assembly.engines :refer :all])
   )
 
-(s/defn start-gen [engine generator :- Generator]
-  (thread
-    (loop [iter (if (> (generator :count) 0) (generator :count) -1)]
-      (if-let [msg ((generator :msg-source))]
-        (do
-          (println (str
-            "Message " msg " ;response "
-            (handle-msg engine msg)
-            ))
-          (if (> (generator :period) 0)
-            (Thread/sleep (generator :period))
-            )
-          (if (= -1 iter)
-            (recur -1)
-            (if-not (= 0 iter)
-              (recur (- iter 1))
-              )
+(s/defn run-gen [engine generator :- Generator]
+  (loop [iter (if (> (generator :count) 0) (generator :count) -1)]
+    (if-let [msg ((generator :msg-source))]
+      (do
+        (println (str
+          "Message " msg " ;response "
+          (handle-msg engine msg)
+          ))
+        (if (> (generator :period) 0)
+          (Thread/sleep (generator :period))
+          )
+        (if (= -1 iter)
+          (recur -1)
+          (if-not (= 0 iter)
+            (recur (- iter 1))
             )
           )
         )
@@ -30,5 +27,6 @@
   )
 
 (s/defn start-all-gens [engine generators :- [Generator]]
-  (map #(start-gen engine %) generators)
+  ;We can rewrite it to have more control on future (to cancel or suspend gens)
+  (doall (pmap #(run-gen engine %) generators))
   )
