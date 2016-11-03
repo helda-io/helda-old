cad7069821d47dbb86767a29a4d0f21863ec88c2
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2016-11-03 20:56:38 +0300

    Filtering implemented

--- src/helda/meta/worlds.clj
+++ src/helda/meta/worlds.clj
@@ -4,6 +4,18 @@
   (:require [schema.core :as s])
   )
 
+(defn trim-handlers [world-meta world-tag]
+  (let [
+    handlers-map (get-in world-meta [:worlds-meta world-tag :handlers])
+    tags (keys handlers-map)
+  ]
+    (zipmap
+      tags
+      (map #(dissoc (handlers-map %) :tag :handler) tags)
+      )
+    )
+  )
+
 ; Worlds meta-info app
 (defn create-meta [meta-list]
   (-> (init-meta :worlds-meta)
@@ -21,14 +33,15 @@
 
     (add-handler {
       :tag :worlds
-      :input-msg {
-        }
       :handler (fn [msg world]
-        (reply-msg {} {
+        (reply-msg
+          (init-response :help)
+          {
           :worlds (zipmap
             (world :worlds-list)
-            (-> (map #(get-in world [:worlds-meta % :handlers]) (world :worlds-list))
-              
+            (map
+              #(trim-handlers world %)
+              (world :worlds-list)
               )
             )
           })
